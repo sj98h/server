6b89c174f7e532702170df52127c6904e7295e1e
sj98h <sj98h@naver.com>
2024-08-08 11:43:20 +0900

    서버

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const cors = require("cors");
 const client_id = "7o0NAUXXbSqkZuofNVB2"; // 클라이언트 ID
 const client_secret = "oWwoIOBQIN"; // 클라이언트 시크릿
 const api_url = "https://openapi.naver.com/v1/search/news";
-const query = "검색"; // 검색할 뉴스 키워드
+const keyword = "검색"; // 검색할 뉴스 키워드
 
-async function fetchNews(query = "게임") {
+async function fetchNews(query = "코딩") {
   try {
-    let url = `${api_url}?query=${encodeURIComponent(query)}`;
+    const url = `${api_url}?query=${encodeURIComponent(query)}`;
     const response = await axios.get(url, {
       headers: {
         "X-Naver-Client-Id": client_id,
@@ -20,17 +20,17 @@ async function fetchNews(query = "게임") {
     return response.data;
   } catch (error) {
     console.error(error);
-  } finally {
-    console.log("야이");
+    throw error;
   }
 }
 server.use(cors());
 server.get("/news", async (req, res) => {
   try {
     const result = await fetchNews();
-    return res.json({ result });
+    // console.log(res.json({ result }));
+    return res.json(result);
   } catch (error) {
-    console.error(error);
+    return res.json({ error });
   }
 });
 server.listen(9999, () => console.log("server run"));
